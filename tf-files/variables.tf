7f12fe7562155675ad4dad8c815ad4a84f4d061b
artillery <artillerytrigger@gmail.com>
2024-10-24 22:56:10 +0200

    second

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "ArtilleryTrigger"
+}
+
+variable "git-token" {
+  default = "********"
+}
+
+variable "key-name" {
+  default = "secondkey"
+}
+
+variable "hosted-zone" {
+  default = "devopsyavuz.com"
+}
